f0fabc2e4f0b514c2d5949eea05dc3bfb86c243d
OatPakin <kaoatheremoo@gmail.com>
2024-09-03 20:34:31 +0700

    ma leaw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Pakin Niramitsima"
+            studentId="660610782"
           />
         </div>
       </body>
